c73b5bd85aaf14d0d7e93d3c6364a53a12b9e40b
Frank Aceves <frankaceves@gmail.com>
2019-09-05 19:40:55 -0700

    add new mapForItinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Frank Aceves on 9/5/19.
+//  Copyright © 2019 Frank Aceves. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
